d186799cfbc1cf1832922ef65c989a64db47e2f6
Matheus Valença <matheusniklaus@gmail.com>
2022-05-28 10:28:00 -0300

    criando função para reproduzir som ao click

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+
 const criarDiv = (texto)=>{
     const div = document.createElement('div')
     div.textContent = texto
@@ -5,3 +6,34 @@ const criarDiv = (texto)=>{
     div.id = texto
     document.getElementById('container').appendChild(div)
 }
+
+const sons = {
+    'A':'boom.wav',
+    'S':'clap.wav',
+    'D': 'hihat.wav',
+    'F': 'kick.wav',
+    'G': 'openhat.wav',
+    'H': 'ride.wav',
+    'J': 'snare.wav',
+    'K': 'tink.wav',
+    'L': 'tom.wav'
+}
+
+for(const som in sons) {
+    criarDiv(som)       
+}
+
+const tocarSom = som =>{
+  const novoSom = new Audio(`./assets/sounds/${som}`)
+  novoSom.play()
+}
+
+document.addEventListener('click',e=>{
+ const elemento = e.target
+ if(elemento.classList.contains('key')){
+     tocarSom(sons[elemento.textContent])
+     console.log(sons[elemento.textContent])
+ }
+ return
+})
+
